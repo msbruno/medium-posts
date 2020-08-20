1b152ea1b88e044706e96a51f16cacc7a8470e14
Bruno Silva <ms.bruno1@gmail.com>
2020-08-20 17:45:10 -0300

    Update and rename refactoring/ReplaceTempWithQueryBefore.java to refactoring/ReplaceTempWithQuery/Before.java

---
+++ refactoring/ReplaceTempWithQuery/Before.java
@@ -0,0 +1,25 @@
+class Calculator() {
+
+  int quantity;
+  double itemPrice;
+
+  public static void main(String[] args){
+    System.out.println(new Calculator(10, 100d));
+  }
+
+  Calculadora(int quantity, double itemPrice){
+    this.quantity = quantity;
+    this.itemPrice = itemPrice;
+  }
+
+  double calculaTotal() {
+    double basePrice = quantity * itemPrice
+
+    if (basePrice > 1000) {
+      return basePrice * 0.95;
+    }
+    else {
+      return basePrice * 0.98;
+    }
+  }
+}
