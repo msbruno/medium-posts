c98a23eb6791772149badf00d7d9bdf71ab2d121
Bruno Silva <ms.bruno1@gmail.com>
2020-08-20 17:46:08 -0300

    Update and rename refactoring/ReplaceTempWithQueryAfter.java to refactoring/ReplaceTempWithQuery/After.java

--- refactoring/ReplaceTempWithQuery/After.java
+++ refactoring/ReplaceTempWithQuery/After.java
@@ -13,13 +13,15 @@ class Calculator() {
   }
 
   double calculaTotal() {
-    double basePrice = quantity * itemPrice
-
-    if (basePrice > 1000) {
-      return basePrice * 0.95;
+    if (basePrice() > 1000) {
+      return basePrice() * 0.95;
     }
     else {
-      return basePrice * 0.98;
+      return basePrice() * 0.98;
     }
+  }
+  
+  double basePrice(){
+    return quantity * itemPrice;
   }
 }
